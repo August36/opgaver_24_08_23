3f00214ca284462ef05594af96e90063a098efb7
August <augustjp@live.dk>
2023-08-24 10:29:37 +0200

    changed h1, h2 and paragraph text with JS

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const title = document.querySelector("h1");
 const secondTitle = document.querySelector("h2");
 const thirdTitle = document.querySelector("h3");
-const sektion = document.querySelector("section");
+const paragraph = document.querySelector("p");
 const artikel = document.querySelector("article");
+
+title.textContent = "en helt ny titel";
+secondTitle.textContent = "h2 chaaanged";
+paragraph.innerHTML = "<b>new article text</b>";
