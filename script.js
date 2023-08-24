b7790c73697c71fc683632c2b2c766e9f58bc31a
August <augustjp@live.dk>
2023-08-24 10:36:06 +0200

    created a span element and added it to the h1

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ const secondTitle = document.querySelector("h2");
 const thirdTitle = document.querySelector("h3");
 const paragraph = document.querySelector("p");
 const artikel = document.querySelector("article");
+const spanElement = document.createElement("span");
 
 title.textContent = "en helt ny titel";
 secondTitle.textContent = "h2 chaaanged";
-paragraph.innerHTML = "<b>new article text</b>";
+paragraph.createElement = "span";
+
+spanElement.textContent = "nyt nyt hejehje";
+
+title.appendChild(spanElement);
