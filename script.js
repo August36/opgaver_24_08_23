e8615c2d1b18a107da84d43aa296b15dd6bb192d
August <augustjp@live.dk>
2023-08-24 10:22:04 +0200

    chose html elements with document queryselector

---
+++ script.js
@@ -0,0 +1,5 @@
+const title = document.querySelector("h1");
+const secondTitle = document.querySelector("h2");
+const thirdTitle = document.querySelector("h3");
+const sektion = document.querySelector("section");
+const artikel = document.querySelector("article");
